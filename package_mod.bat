7d60f68e8bd086b2ad3076363da48447f5b624cb
CamDawg <pcamagna@yahoo.com>
2025-01-26 14:16:28 -0800

    v7 prep

--- package_mod.bat
+++ package_mod.bat
@@ -6,7 +6,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=The IWD2 NPC Project"
-set mod_version=Test_Build_1_for_v7
+set mod_version=v7
 set mod_folder=iwd2npc
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
